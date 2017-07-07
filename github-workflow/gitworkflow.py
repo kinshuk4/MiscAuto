b32a101947a572efdb9f702420604762e1c7f898
Kinshuk <kinshuk.ram@gmail.com>
2017-07-07 22:27:17 +0200

    Improving the code logic a bit more

--- github-workflow/gitworkflow.py
+++ github-workflow/gitworkflow.py
@@ -2,55 +2,98 @@ import subprocess
 import time
 import sys
 import re
+import json
 
 
 # https://gist.github.com/blackfalcon/8428401
 # https://gist.github.com/CyberShadow/1885859
-def switch_to_master():
-    commands = ["git checkout master", "git pull origin master"]
-    for command in commands:
-        bash_command(command)
+class IssueState:
+    NoBranch = 1
+    BranchCreated = 2
+    FirstCommitDone = 3
+    InFlow = 4
+    Delete = 5
 
 
-def get_new_branch(branchName):
-    switch_to_master()
-    checkout_cmd = "git checkout -b " + branchName
-    bash_command(checkout_cmd)
+class GithubHelper:
+    @staticmethod
+    def sync_master():
+        commands = ["git checkout master", "git pull origin master"]
+        for command in commands:
+            bash_command(command)
+
+    @staticmethod
+    def create_new_branch(branchName):
+        checkout_cmd = "git checkout -b " + branchName
+        bash_command(checkout_cmd)
+
+    @staticmethod
+    def switch_to_branch(branchName):
+        checkout_cmd = "git checkout " + branchName
+        bash_command(checkout_cmd)
+
+    @staticmethod
+    def push_branch(branchName):
+        command = ["git push origin " + branchName]
+        bash_commands(command)
+
+    @staticmethod
+    def commit_change(commit_msg):
+        commands = ["git add --all", 'git commit -m  ' + commit_msg]
+        bash_commands(commands)
+
+    @staticmethod
+    def generate_pr(branchName):
+        url = bash_command(
+            "git config --get remote.origin.url  | sed -e 's#^[^:/]\+:\(//[^/]\+/\)\?#https://github.com/#' -e 's#\.git$##' -e 's#/$##'")
+        print(str(url))
+        GithubHelper.switch_to_branch(branchName)
+        output = bash_command("git pull-request master")
+        return output
+
+    @staticmethod
+    def delete_branch(branchName):
+        bash_command("git branch -d " + branchName)
+
+    @staticmethod
+    def git_sync(branchName):
+        commands = ["git checkout " + branchName, "git pull", "git push origin " + branchName]
+        bash_commands(commands)
 
 
-def switch_to_branch(branchName):
-    checkout_cmd = "git checkout " + branchName
-    bash_command(checkout_cmd)
+class GithubProcessor:
+    def __init__(self, issueNum, issueString):
+        self.issueNum = issueNum
+        self.issueString = issueString
+        issueStringBranch = issueString.lower().replace(" ", "-")
+        self.commitIssue = "ISSUE-" + str(issueNum) + "-"
+        self.branchName = self.commitIssue + str(issueStringBranch)
+        self.state = IssueState.NoBranch
 
+    def create_branch(self):
+        print("Creating the new branch: ", self.branchName)
+        GithubHelper.sync_master()
+        if self.state == IssueState.NoBranch:
+            GithubHelper.create_new_branch(self.branchName)
+            self.state = IssueState.BranchCreated
+        else:
+            self.switch_branch()
 
-def commit_n_push_new_branch(issueNum, issueString, branchName):
-    commitMsg = '"' + 'ISSUE-' + issueNum + ' ' + issueString + '"'
-    commands = ["git add --all", 'git commit -m  ' + commitMsg,
-                "git push origin " + branchName]
-    bash_commands(commands)
+    def commit_n_push(self, message=None):
+        if self.state == IssueState.BranchCreated:
+            yes = prompt_yes("Should I add and commit and push? [y/n]")
+            commit_msg = '"' + 'ISSUE-' + self.issueNum + ' ' + self.issueString + '"'
+            GithubHelper.commit_change(commit_msg)
+            self.state = IssueState.FirstCommitDone
+            GithubHelper.push_branch(self.branchName)
+        elif message is None:
+            raise Exception(self.issueNum + "No message to commit to")
 
+    def switch_branch(self):
+        GithubHelper.switch_to_branch(self.branchName)
 
-def generate_pr(issueNum, branchName):
-    # commands = ["git checkout " + branchName, "git pull origin " + branchName, "git pull origin master",
-    #             "git checkout master", "git pull origin master"]
-    # for command in commands:
-    #     print(bash_command(command))
-    print("here")
-    url = bash_command(
-        "git config --get remote.origin.url  | sed -e 's#^[^:/]\+:\(//[^/]\+/\)\?#https://github.com/#' -e 's#\.git$##' -e 's#/$##'")
-    print(str(url))
-    switch_to_branch(branchName)
-    output = bash_command("git pull-request master")
-    return output
-
-
-def delete_branch(branchName):
-    bash_command("git branch -d " + branchName)
-
-
-def git_sync(branchName):
-    commands = ["git checkout " + branchName, "git pull", "git push origin " + branchName]
-    bash_commands(commands)
+    def generate_pr(self):
+        return GithubHelper.generate_pr(self.branchName)
 
 
 def bash_commands(commands):
@@ -79,7 +122,6 @@ def bash_command(command):
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = process.communicate()[0]
     else:
-        print(split_command)
         process = subprocess.Popen(split_command)
         output = process.communicate()
     return output
@@ -106,30 +148,21 @@ def prompt_yes(message):
 
 
 def workflow(issueNum, issueString):
-    issueStringBranch = issueString.lower().replace(" ", "-")
-    branchName = "ISSUE-" + str(issueNum) + "-" + str(issueStringBranch)
+    p = GithubProcessor(issueNum, issueString)
+    p.create_branch()
+    p.commit_n_push()
 
-    print("Checking out the new branch: ", branchName)
-    # get_new_branch(branchName)
-    switch_to_branch(branchName)
-    git_sync(branchName)
-    print("Switched to new branch: {}. Please start coding and let me know when I can commit", branchName)
-    # yes = prompt_yes("Should I add and commit? [y/n]")
-    #
-    # if yes:
-    #     commit_n_push_new_branch(issueNum, issueString, branchName)
-
-    option = input("Pull request: 1 , commit: 2, delete: 3 , done: 4?")
+    option = input("Pull request: 1 , commit: 2, delete: 3 , done: 4? ")
     while option is not "3" or "4":
         if option is "1":
-            output = generate_pr(issueNum, branchName)
+            output = p.generate_pr()
             print(output)
         elif option is "2":
             output = input("Please tell message for commit: ")
-            commit_n_push_new_branch(issueNum, output, branchName)
+            p.commit_n_push(output)
         elif option is "3":
             print("Deleting the branch.")
-            delete_branch
+            # delete_branch
         elif option is "4":
             print("we are done now")
 
@@ -141,11 +174,11 @@ def main():
 
 
 def test():
-    issueNum = "ddd"
-    issueString = "adafad adafda"
+    issueNum = "60"
+    issueString = "Deploy fallback Solrs to eu-west-1"
     workflow(issueNum, issueString)
 
 
 if __name__ == '__main__':
-    # main()
-    test()
+    main()
+    # test()
